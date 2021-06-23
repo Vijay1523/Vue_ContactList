f3b16393844a966dee89b44e2bd77c234f4ff15a
VIJAY_PERSONAL_\Vijay Vaghani <vijayvaghani1@gmail.com>
2021-06-22 22:24:02 -0700

    Loader code formatted.

--- src/components/common/Loader.vue
+++ src/components/common/Loader.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="text-center text-danger">
-        <b-spinner class="align-middle"></b-spinner>
-        <strong>Loading...</strong>
-    </div>
+  <div class="text-center text-danger">
+    <b-spinner class="align-middle"></b-spinner>
+    <strong>Loading...</strong>
+  </div>
 </template>
 
+<script>
 export default {
-  name: 'Loader',
-  data () {
-    return {
-    }
+  name: "Loader",
+  data() {
+    return {};
   }
-}
+};
 </script>
